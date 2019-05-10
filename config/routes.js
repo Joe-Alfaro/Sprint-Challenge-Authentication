b3478964f56e9a7cd0dc88821455692533105706
joe-alfaro <josephlalfaro@gmail.com>
2019-05-10 09:36:19 -0700

    implement login with jwt tokens

--- config/routes.js
+++ config/routes.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
-const { authenticate } = require('../auth/authenticate');
+const { authenticate, jwtKey } = require('../auth/authenticate');
 const Users = require('../users/users-model.js');
-
 
 module.exports = server => {
   server.post('/api/register', register);
@@ -30,7 +30,37 @@ function register(req, res) {
 };
 
 function login(req, res) {
-  // implement user login
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      console.log(user)
+      if(user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        console.log('in if statement');
+        res
+          .status(200)
+          .json({
+            message: 'login successful',
+            token
+          });
+      }
+      else {
+        res
+          .status(401)
+          .json({ 
+            message: 'Invalid Credential' 
+          });
+      }
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({
+          errorMessage: 'login error' 
+        });
+    }); 
 }
 
 function getJokes(req, res) {
@@ -46,4 +76,17 @@ function getJokes(req, res) {
     .catch(err => {
       res.status(500).json({ message: 'Error Fetching Jokes', error: err });
     });
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  
+  const options = {
+    expiresIn: '1h'
+  };
+  
+  return jwt.sign(payload, jwtKey, options);
 }
